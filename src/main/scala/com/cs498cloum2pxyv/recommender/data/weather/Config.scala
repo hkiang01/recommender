e477c25c11e76314a9fd5965d04862a4371c9b26
harry <hkiang01@gmail.com>
2018-03-07 19:17:26 -0500

    weather import works

--- src/main/scala/com/cs498cloum2pxyv/recommender/data/weather/Config.scala
+++ src/main/scala/com/cs498cloum2pxyv/recommender/data/weather/Config.scala
@@ -2,24 +2,6 @@ package com.cs498cloum2pxyv.recommender.data.weather
 
 object Config {
 
-  val noaaChicagoDailyTempFields: Array[String] = Array(
-    "station",
-    "name",
-    "state",
-    "country",
-    "date",
-    "tmax",
-    "tmin",
-    "tobs"
-  )
-  case class NoaaChicagoDailyTemp(
-    station: String,
-    name: String,
-    state: String,
-    country: String,
-    date: String,
-    tmax: String,
-    tmin: String,
-    tobs: String)
+
 
 }
--- src/main/scala/com/cs498cloum2pxyv/recommender/data/weather/Ingestion.scala
+++ src/main/scala/com/cs498cloum2pxyv/recommender/data/weather/Ingestion.scala
@@ -2,24 +2,44 @@ package com.cs498cloum2pxyv.recommender.data.weather
 
 import java.io.File
 
-import com.cs498cloum2pxyv.recommender.data.weather.Config._
+import com.cs498cloum2pxyv.recommender.ApplicationExecutionEnvironment
 import org.apache.flink.api.scala.{DataSet, ExecutionEnvironment}
+import org.apache.flink.streaming.api.scala._
+
 
 object Ingestion {
 
-  def csvFile: File = {
-    new File("src/main/resources/noaa_chicago_daily_temp.csv")
-  }
+  val noaaChicagoDailyTempFields: Array[String] = Array(
+    "station",
+    "name",
+    "state",
+    "country",
+    "date",
+    "tmax",
+    "tmin",
+    "tobs"
+  )
+  case class NoaaChicagoDailyTemp(
+                                   station: String,
+                                   name: String,
+                                   state: String,
+                                   country: String,
+                                   date: String,
+                                   tmax: String,
+                                   tmin: String,
+                                   tobs: String)
+
+  def csvFile: File = new File("src/main/resources/noaa_chicago_daily_temp.csv")
 
   def data(env: ExecutionEnvironment): DataSet[NoaaChicagoDailyTemp] = {
     env.readCsvFile[NoaaChicagoDailyTemp](
       csvFile.getAbsolutePath,
-      pojoFields = Config.noaaChicagoDailyTempFields)
+      pojoFields = noaaChicagoDailyTempFields)
   }
 
   def main(args: Array[String]): Unit = {
     println(csvFile.getAbsolutePath)
-//    val env = ApplicationExecutionEnvironment.env
-//    println(s"${noaaChicagoDailyTempData(env).count()} temp observations")
+    val env = ApplicationExecutionEnvironment.env
+    println(s"${data(env).count()} temp observations")
   }
 }
