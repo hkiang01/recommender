97e4c60e3e8a81e91d1d46a9ede847dee35d4504
harry <hkiang01@gmail.com>
2018-04-16 20:33:08 -0400

    pitiful score

--- ML/input/svm.py
+++ ML/input/svm.py
@@ -3,6 +3,7 @@ from sklearn import svm
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+import pickle
 
 INPUTFILE_PROCESSED=''
 
@@ -11,7 +12,9 @@ def main():
     features = pd.read_csv('ML/data_features.csv')
     labels = pd.read_csv('ML/data_labels.csv')
 
-    x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.2)
+    x_large, x_small, y_large, y_small = train_test_split(features, labels, test_size=0.05, random_state=12345)
+
+    x_train, x_test, y_train, y_test = train_test_split(x_small, y_small, test_size=0.2, random_state=45678)
     print(x_train.shape, y_train.shape)
     print(x_test.shape, y_test.shape)
 
@@ -19,6 +22,10 @@ def main():
     clf.fit(x_train, y_train)
 
     predictions = clf.predict(x_test)
+
+    y_test.to_csv('y_test.csv')
+    predictions.to_csv('predictions.csv')
+
     score = accuracy_score(y_test, predictions)
     print(score)
 
