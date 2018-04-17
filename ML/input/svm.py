0533c017f9c4ae2f6e759acf46aa44d4fbf31edd
harry <hkiang01@gmail.com>
2018-04-17 00:30:13 -0400

    apparently weather makes things worse?
    I'd say 19% prediction for 300 unique classes is pretty alright

--- ML/input/svm.py
+++ ML/input/svm.py
@@ -6,45 +6,62 @@ from sklearn.metrics import accuracy_score
 import pickle
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
+from scipy.stats import itemfreq
 
-def train_and_predict(clf, x_train, y_train, x_test, y_test):
+def train_and_predict(clf, x_train, x_test, y_train, y_test):
     clf = clf.fit(x_train, y_train)
     predictions = clf.predict(x_test)
     score = accuracy_score(y_test, predictions)
     return score
 
-def main():
-
-    features = pd.read_csv('ML/data_features.csv')
-    labels = pd.read_csv('ML/data_labels.csv')
-
-    x_large, x_small, y_large, y_small = train_test_split(features, labels, test_size=0.3)
-
-    x_train, x_test, y_train, y_test = train_test_split(x_small, y_small, test_size=0.2)
+def run_through_models(x_train, x_test, y_train, y_test):
     print(x_train.shape, y_train.shape)
     print(x_test.shape, y_test.shape)
+    item_freq = itemfreq(y_test)
+    print(f"there are {len(item_freq)} unique classes in the test set")
 
+    # takes a LONG time
     # clf = svm.SVC()
     # clf.fit(x_train, y_train)
     # predictions = clf.predict(x_test)
 
     for n_estimators in range(3,15,1):
         clf = ensemble.RandomForestClassifier(n_estimators,max_depth=None,)
-        score = train_and_predict(clf, x_train, y_train, x_test, y_test)
+        score = train_and_predict(clf, x_train, x_test, y_train, y_test)
         print(f"model: random forest, n_estimators: {n_estimators}, score: {score}")
 
     for weights in ['uniform', 'distance']:
         for n_neighbors in range(15,3,-1):
             clf = neighbors.KNeighborsClassifier(n_neighbors,weights)
-            score = train_and_predict(clf, x_train, y_train, x_test, y_test)
+            score = train_and_predict(clf, x_train, x_test, y_train, y_test)
             print(f"model: knn, weights: {weights}, n_neighbors: {n_neighbors}, score: {score}")
 
     clf = tree.DecisionTreeClassifier()
-    score = train_and_predict(clf, x_train, y_train, x_test, y_test)
-    print("model: decision tree, score: {score}")
+    score = train_and_predict(clf, x_train, x_test, y_train, y_test)
+    print(f"model: decision tree, score: {score}")
 
-    # pd.DataFrame(y_test).to_csv('ML/y_test.csv')
-    # pd.DataFrame(predictions).to_csv('ML/predictions.csv')
+def main():
+
+    features = pd.read_csv('ML/data_features.csv', header=None)
+    labels = pd.read_csv('ML/data_labels.csv', header=None)
+
+    # with all features
+    print("BEGIN ALL FEATURES")
+    x_large, x_small, y_large, y_small = train_test_split(features, labels, test_size=0.9)
+    x_train, x_test, y_train, y_test = train_test_split(x_small, y_small, test_size=0.2)
+    run_through_models(x_train, x_test, y_train, y_test)
+    print("END ALL FEATURES")
+
+    print()
+    print()
+    print()
+
+    # sans weather
+    print("BEGIN WITHOUT WEATHER")
+    x_train = x_train.drop([6,7,8],axis=1)
+    x_test = x_test.drop([6,7,8],axis=1)
+    run_through_models(x_train, x_test, y_train, y_test)
+    print("END WITHOUT WEATHER")  
 
 if __name__ == "__main__":
     main()
