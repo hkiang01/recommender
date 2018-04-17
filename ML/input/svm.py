4f73f6432d8b56b203cba0118c5682a65ec8643c
harry <hkiang01@gmail.com>
2018-04-17 00:36:16 -0400

    print item_freq, only use decision tree

--- ML/input/svm.py
+++ ML/input/svm.py
@@ -18,6 +18,7 @@ def run_through_models(x_train, x_test, y_train, y_test):
     print(x_train.shape, y_train.shape)
     print(x_test.shape, y_test.shape)
     item_freq = itemfreq(y_test)
+    print(item_freq)
     print(f"there are {len(item_freq)} unique classes in the test set")
 
     # takes a LONG time
@@ -25,17 +26,18 @@ def run_through_models(x_train, x_test, y_train, y_test):
     # clf.fit(x_train, y_train)
     # predictions = clf.predict(x_test)
 
-    for n_estimators in range(3,15,1):
-        clf = ensemble.RandomForestClassifier(n_estimators,max_depth=None,)
-        score = train_and_predict(clf, x_train, x_test, y_train, y_test)
-        print(f"model: random forest, n_estimators: {n_estimators}, score: {score}")
+    # for n_estimators in range(3,15,1):
+    #     clf = ensemble.RandomForestClassifier(n_estimators,max_depth=None,)
+    #     score = train_and_predict(clf, x_train, x_test, y_train, y_test)
+    #     print(f"model: random forest, n_estimators: {n_estimators}, score: {score}")
 
-    for weights in ['uniform', 'distance']:
-        for n_neighbors in range(15,3,-1):
-            clf = neighbors.KNeighborsClassifier(n_neighbors,weights)
-            score = train_and_predict(clf, x_train, x_test, y_train, y_test)
-            print(f"model: knn, weights: {weights}, n_neighbors: {n_neighbors}, score: {score}")
+    # for weights in ['uniform', 'distance']:
+    #     for n_neighbors in range(15,3,-1):
+    #         clf = neighbors.KNeighborsClassifier(n_neighbors,weights)
+    #         score = train_and_predict(clf, x_train, x_test, y_train, y_test)
+    #         print(f"model: knn, weights: {weights}, n_neighbors: {n_neighbors}, score: {score}")
 
+    # seems to perform the best
     clf = tree.DecisionTreeClassifier()
     score = train_and_predict(clf, x_train, x_test, y_train, y_test)
     print(f"model: decision tree, score: {score}")
